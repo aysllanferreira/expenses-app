21bfbfe50b5d687f3f4df9c917f3b834404f6dcb
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-15 18:28:03 -0300

    att lint

--- prettierrc.js
+++
@@ -1,9 +0,0 @@
-// npm i -D prettier eslint-config-prettier eslint-plugin-prettier
-
-module.exports = {
-  semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 2,
-};
