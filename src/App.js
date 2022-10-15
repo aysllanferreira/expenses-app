76bb43760a57e3d9967c6eec53323f4d885308ea
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-15 10:04:22 -0300

    expenseItem created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ExpenseItem from './Components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <ExpenseItem />
     </div>
   );
 }
