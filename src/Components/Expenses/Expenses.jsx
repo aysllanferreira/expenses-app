c9c3bd5bbaf67659315e626f3ad78979c72a3852
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-15 16:08:22 -0300

    Creating filter listeners

--- src/Components/Expenses/Expenses.jsx
+++ src/Components/Expenses/Expenses.jsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpenseFilter';
 import './Expenses.css'
 
 const Expenses = ({ expenses }) => {
+
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+
+  };
+
   return (
-    <Card className='expenses'>
-        {expenses.map(({ id, title, amount, date }) => (
-        <ExpenseItem key={id} title={title} amount={amount} date={date} />
-      ))}
-    </Card>
+    <div>
+      <Card className='expenses'>
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterHandler} />
+          {expenses.map(({ id, title, amount, date }) => (
+          <ExpenseItem key={id} title={title} amount={amount} date={date} />
+        ))}
+      </Card>
+    </div>
   )
 }
 
