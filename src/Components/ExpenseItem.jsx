d948d7a5cd310ebb37bdeffa111707e1da875187
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-15 11:48:12 -0300

    Creating a global style to Cards

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 import './ExpenseItem.css'
 
 function ExpenseItem({ id, title, amount, date }) {
 
   return (
-    <div id={id} className='expense-item'>
+    <Card id={id} className='expense-item'>
       <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>{amount}</div>
       </div>
-    </div>
+    </Card>
   )
 }
 
--- src/Components/Expenses.jsx
+++ src/Components/Expenses.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 import './Expenses.css'
 
 function Expenses({ expenses }) {
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
         {expenses.map(({ id, title, amount, date }) => (
         <ExpenseItem id={id} title={title} amount={amount} date={date} />
       ))}
-    </div>
+    </Card>
   )
 }
 
