9ff10c0ed55fbf9bafc484051c1302209727da16
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-15 10:22:12 -0300

    ExpenseItem data becoming dynamic

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+
+  const getDate = new Date(2022, 2, 28);
+  const expenseTitle = `Car Insurance`;
+  const expenseAmount = 294.67;
+
   return (
     <div className='expense-item'>
-      <div>March 28 2022</div>
+      <div>{getDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>{expenseAmount}</div>
       </div>
     </div>
   )
