6ec24e2053dce536d8a44104bcc3ec3713d146c1
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-15 10:15:42 -0300

    css applied into the file ExpenseItem

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -3,11 +3,11 @@ import './ExpenseItem.css'
 
 function ExpenseItem() {
   return (
-    <div>
+    <div className='expense-item'>
       <div>March 28 2022</div>
-      <div>
+      <div className='expense-item__description'>
         <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <div className='expense-item__price'>$294.67</div>
       </div>
     </div>
   )
