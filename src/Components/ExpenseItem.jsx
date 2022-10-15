b8e74e0c695253940a699a7b9a069c6b468b9f86
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-15 11:15:08 -0300

    Spliting Date to ExpenseDate component and styling it

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 function ExpenseItem({ id, title, amount, date }) {
 
-  const month = date.toLocaleString('en-US', { month: 'long' });
-  const year = date.getFullYear();
-  const day = date.toLocaleString('en-US', { day: '2-digit' });
-
   return (
     <div id={id} className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>{amount}</div>
